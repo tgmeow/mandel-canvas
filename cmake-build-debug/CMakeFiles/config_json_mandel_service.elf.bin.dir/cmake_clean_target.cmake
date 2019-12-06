c786edf21db419686792ed93366f37d005dea579
Tiger Mou <tgmeow16@gmail.com>
2019-12-06 23:25:54 +0000

    remove cmake-build-debug

--- cmake-build-debug/CMakeFiles/config_json_mandel_service.elf.bin.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "lib/libconfig_json_mandel_service.elf.bin.a"
-)
--- cmake-build-debug/CMakeFiles/mandel_service.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/mandel_service"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mandel_service.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
